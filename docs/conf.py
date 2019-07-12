2026f91648ccfa462714792bd99378e86dcbe08c
Josh English <joshua.r.english@gmail.com>
2019-07-12 11:40:28 -0700

    fixed docs

--- docs/conf.py
+++ docs/conf.py
@@ -53,3 +53,11 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+source_suffix = '.txt'
+master_doc = 'index'
+
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+html_static_path = ['_static']
+
+
